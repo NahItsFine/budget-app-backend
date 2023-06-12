d16493edb3dd0d61406498ccb6452e6983fdbdb0
JJ Kanu <jjkanu20@gmail.com>
2023-06-12 19:16:03 -0400

    Added keytool, in the middle of setting up controller. Left off at filterchain

--- src/main/java/com/nahitsfine/budgetapp/BudgetappApplication.java
+++ src/main/java/com/nahitsfine/budgetapp/BudgetappApplication.java
@@ -1,16 +1,17 @@
 package com.nahitsfine.budgetapp;
 
+import com.nahitsfine.budgetapp.controllers.TransactionTypeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.nahitsfine.budgetapp.constants.TestConstants;
+import com.nahitsfine.budgetapp.constants.TempConstant;
 
 @SpringBootApplication
 public class BudgetappApplication {
 
-	public BudgetappApplication () {
-		System.out.println("HELLO");
-	}
+//	public BudgetappApplication () {
+//		System.out.println("HELLO");
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetappApplication.class, args);
--- src/main/java/com/nahitsfine/budgetapp/controllers/TransactionTypeController.java
+++ src/main/java/com/nahitsfine/budgetapp/controllers/TransactionTypeController.java
@@ -1,4 +1,13 @@
 package com.nahitsfine.budgetapp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TransactionTypeController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "It iiiiiissss what it issssss.";
+    }
 }
