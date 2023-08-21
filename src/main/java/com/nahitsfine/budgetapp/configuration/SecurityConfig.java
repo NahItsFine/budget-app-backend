a9bd8b04571d2172e90be01b63c9a8138f4d0ce5
JJ Kanu <jjkanu20@gmail.com>
2023-08-21 18:48:11 -0400

    modified gradle

--- src/main/java/com/nahitsfine/budgetapp/configuration/SecurityConfig.java
+++ src/main/java/com/nahitsfine/budgetapp/configuration/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.nahitsfine.budgetapp.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -16,4 +18,4 @@ public class SecurityConfig {
                 .build();
     }
 
-}}
+}
